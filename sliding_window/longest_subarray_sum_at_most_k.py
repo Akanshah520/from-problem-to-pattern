8a09a6d35d6da2030708d4e5034554297ac25d41
Akansha Sharma <151682901+Akanshah520@users.noreply.github.com>
2026-01-08 01:21:56 +0530

    Add files via upload

---
+++ sliding_window/longest_subarray_sum_at_most_k.py
@@ -0,0 +1,15 @@
+def longest_subarray_sum_at_most_k(nums, k):
+    left = 0
+    window_sum = 0
+    max_len = 0
+
+    for right in range(len(nums)):
+        window_sum += nums[right]
+
+        while window_sum > k:
+            window_sum -= nums[left]
+            left += 1
+
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
