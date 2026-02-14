f1813c6e8872133e3eea50851edf74ed2a502e48
Akansha Sharma <151682901+Akanshah520@users.noreply.github.com>
2026-02-14 11:54:13 +0530

    Add files via upload

---
+++ heap_priority_queue/find_median.py
@@ -0,0 +1,24 @@
+import heapq
+
+class MedianFinder:
+    def __init__(self):
+        self.small = []  # max-heap (store negatives)
+        self.large = []  # min-heap
+
+    def add_num(self, num):
+        heapq.heappush(self.small, -num)
+
+        # Ensure ordering property
+        if self.small and self.large and (-self.small[0] > self.large[0]):
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+
+        # Balance sizes
+        if len(self.small) > len(self.large) + 1:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+        if len(self.large) > len(self.small):
+            heapq.heappush(self.small, -heapq.heappop(self.large))
+
+    def find_median(self):
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        return (-self.small[0] + self.large[0]) / 2
